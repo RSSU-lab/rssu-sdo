64cdc147ba4cdebf998f9537a3cca9f53ac27e5e
Shchukin Fyodor <ted888@ya.ru>
2017-11-19 13:41:00 +0300

    Base and Bar added

---
+++ src/components/Bar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="bar-wrapper">
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AppBar',
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .bar-wrapper {
+    position: absolute;
+    
+    width: 90%;
+    height: 150px;
+    
+    left: 5%;
+    bottom: 0px;
+    
+    border-radius: 10px 10px 0px 0px;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+</style>
---
+++ src/components/Base.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="base-wrapper">
+    
+    <app-bar></app-bar>
+  </div>
+</template>
+
+<script>
+  import AppBar from '@/components/Bar.vue'
+
+  export default {
+    name: 'AppBase',
+    components: {
+      AppBar
+    },
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .base-wrapper {
+    position: absolute;
+  
+    left: 0px;
+    right: 0px;
+  
+    top: 6rem;
+    bottom: 0px;
+    
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+</style>
