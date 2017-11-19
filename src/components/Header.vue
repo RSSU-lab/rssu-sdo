6b3bf21b1c29ffe5071c6a3f31aa3e38ec93beb0
SkyReMore <skyremore@mail.ru>
2017-11-19 19:48:30 +0300

    Added scss processing

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -25,44 +25,49 @@
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
   .header-wrapper {
     height: 6rem;
     font-size: 0.95rem;
     /* TODO: select correct color */
     background-color: #7d97ad;
-  }
-  .logo {
-    padding: 1rem;
-    float: left;
-  }
-  .logo img {
-    height: 4rem;
-  }
-  .navbar .nav-list {
-    float: left;
-    width: 75%;
-    margin: 0;
-    padding: 0;
-    list-style: none;
-  }
-  .navbar .nav-list li {
-    display: inline-block;
-    padding: 0 1.2rem;
-    color: #fff;
-    line-height: 6rem;
-    cursor: pointer;
-  }
-  .login-btn {
-    float: right;
-    height: 3rem;
-    margin: 1.5rem;
-    padding: 0 2rem;
-    font-size: inherit;
-    border: 0;
-    border-radius: 0.4rem;
-    color: #000;
-    background-color: #fff;
-    cursor: pointer;
+
+    .logo {
+      padding: 1rem;
+      float: left;
+
+      img {
+        height: 4rem;
+      }
+    }
+    .navbar {
+      .nav-list {
+        float: left;
+        width: 75%;
+        margin: 0;
+        padding: 0;
+        list-style: none;
+
+        li {
+          display: inline-block;
+          padding: 0 1.2rem;
+          color: #fff;
+          line-height: 6rem;
+          cursor: pointer;
+        }
+      }
+      .login-btn {
+        float: right;
+        height: 3rem;
+        margin: 1.5rem;
+        padding: 0 2rem;
+        font-size: inherit;
+        border: 0;
+        border-radius: 0.4rem;
+        color: #000;
+        background-color: #fff;
+        cursor: pointer;
+      }
+    }
   }
 </style>
